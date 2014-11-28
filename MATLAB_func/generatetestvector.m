80c8cdf06471f422737eed12535283d2205a8bc8
unknown <Lab@MC2-M237.net.chalmers.se>
2014-11-28 11:36:10 +0100

    Backup

--- MATLAB_func/generatetestvector.m
+++ MATLAB_func/generatetestvector.m
@@ -1,15 +1,15 @@
-Barker = [1 1 1 1 1 0 0 1 1 0 1 0 1];
+%Barker = [1 1 1 1 1 0 0 1 1 0 1 0 1];
 
-for i = 1:length(Barker)
-   if Barker(i) == 1
-       barker_sym(4*i-3:4*i) = [1 0 0 0];
-   else
-       barker_sym(4*i-3:4*i) = [0 0 1 0];
-   end
-end
+% for i = 1:length(Barker)
+%    if Barker(i) == 1
+%        barker_sym(4*i-3:4*i) = [1 0 0 0];
+%    else
+%        barker_sym(4*i-3:4*i) = [0 0 1 0];
+%    end
+% end
 
 message = randi([0 1],1e4,1);
 
-bits = [barker_sym message'];
+bits = [message'];
 
 save('testvector.mat','bits');
