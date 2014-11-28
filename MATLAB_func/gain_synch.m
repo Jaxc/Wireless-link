bc55382aac9d73e97617db134dc6b8711abefb23
unknown <Lab@MC2-M237.net.chalmers.se>
2014-11-28 08:39:56 +0100

    Backup

--- MATLAB_func/gain_synch.m
+++ MATLAB_func/gain_synch.m
@@ -6,7 +6,7 @@ P_mean(1) = mean(abs(rt_noise).^2);
 for i = 1:length(rt_norm)-1
     sample = rt_noise(i);
     P_mean(i+1) = (1-mu)*P_mean(i)+mu*abs(sample).^2;
-    rt_norm(i) = sample/sqrt(P_mean(i+1))*0.9;
+    rt_norm(i) = sample/sqrt(P_mean(i+1))*sqrt(0.9);
 end
 
 e = zeros(length(rt_noise),1);
--- MATLAB_func/gardner_symbol_recovery.m
+++ MATLAB_func/gardner_symbol_recovery.m
@@ -1,4 +1,4 @@
-function [ symbol ] = gardner_symbol_recovery( samples, sps )
+function [ symbol offsetvec ] = gardner_symbol_recovery( samples, sps )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 error=1;
@@ -8,13 +8,18 @@ offsetvec=zeros(1,floor(numel(samples)/sps-1));
 thesame=0;
 for outer=1:floor(numel(samples)/sps-1)
     current_start=(outer-1)*sps+offset;
-    symbol(outer)=samples(current_start);
+    
     if thesame>15
-        k=0.0001*8*8;
+        %k=0.0001*8*8;
+        k = 0.00001*8*8;
     else
-        k=0.02*8*8;
+        %k=0.02*8*8;
+        k = 0.05*8*8;
     end
-    error=error+k*((samples(current_start+sps/2)-mean([samples(current_start) samples(current_start+sps)]))*(samples(current_start)-samples(current_start+sps)));
+    if current_start+sps<=numel(samples)
+        symbol(outer)=samples(current_start);
+        error=error+k*((samples(current_start+sps/2)-mean([samples(current_start) samples(current_start+sps)]))*(samples(current_start)-samples(current_start+sps)));
+    end
     if offset==round(error)
         thesame=thesame+1;
     else
